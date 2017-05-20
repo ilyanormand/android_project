0015afe9d8bcdc8d02fb8f3d181d0ec4d64171c6
ilyanormand <ilya@1-tn.com>
2017-05-20 21:18:28 +0200

    v1

---
+++ app/src/main/java/com/root/project_rozisk2/MainActivity.java
@@ -0,0 +1,107 @@
+package com.root.project_rozisk2;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+
+
+
+
+
+public class MainActivity extends Activity {
+
+    final int CAMERA_CAPTURE = 1;
+    final int PIC_CROP = 2;
+    private Uri picUri;
+    final String TAG = "myLogs";
+    private File directory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void onClickPhoto(View v) {
+        try {
+            // Намерение для запуска камеры
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(captureIntent, CAMERA_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            // Выводим сообщение об ошибке
+            String errorMessage = "Ваше устройство не поддерживает съемку";
+            Toast toast = Toast
+                    .makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            // Вернулись от приложения Камера
+            if (requestCode == CAMERA_CAPTURE) {
+                // Получим Uri снимка
+                picUri = data.getData();
+                // кадрируем его
+                performCrop();
+            }
+            // Вернулись из операции кадрирования
+            else if (requestCode == PIC_CROP) {
+                Bundle extras = data.getExtras();
+                // Получим кадрированное изображение
+                Bitmap thePic = extras.getParcelable("data");
+                // передаём его в ImageView
+                ImageView picView = (ImageView) findViewById(R.id.picture);
+                picView.setImageBitmap(thePic);
+
+
+
+            }
+
+        }
+    }
+
+    private void performCrop() {
+        try {
+            // Намерение для кадрирования. Не все устройства поддерживают его
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(picUri, "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 256);
+            cropIntent.putExtra("outputY", 256);
+            cropIntent.putExtra("return-data", true);
+            startActivityForResult(cropIntent, PIC_CROP);
+
+
+        } catch (ActivityNotFoundException anfe) {
+            String errorMessage = "Извините, но ваше устройство не поддерживает кадрирование";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
